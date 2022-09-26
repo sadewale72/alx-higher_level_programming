d578c1dde8aa3414b5b8e48c6a4b28267e2286ec
“sadewale72” <adewaleaderoju202@yahoo.com>
2022-09-26 02:21:32 -0700

    My seventh commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,10 +2,6 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    """Print a matrix of integers."""
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-                print("{:d}".format(matrix[i][j]), end="")
-                if j != (len(matrix[i]) - 1):
-                    print(" ", end="")
-        print("")
+    if matrix is None:
+        return None
+    print('\n'.join([' '.join(['{:d}'.format(x) for x in y]) for y in matrix]))
