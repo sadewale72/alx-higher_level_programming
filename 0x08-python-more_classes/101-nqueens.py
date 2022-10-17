927c061aef7bd784af27ac7c1fdbfa02d72ec8c1
“sadewale72” <adewaleaderoju202@yahoo.com>
2022-10-17 06:44:05 -0700

    My eleventh commit

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -27,11 +27,13 @@ def init_board(n):
     [row.append(' ') for i in range(n) for row in board]
     return (board)
 
+
 def board_deepcopy(board):
     """Return a deepcopy of a chessboard."""
     if isinstance(board, list):
         return list(map(board_deepcopy, board))
     return (board)
+
 
 def get_solution(board):
     """Return the list of lists representation of a solved chessboard."""
@@ -42,6 +44,7 @@ def get_solution(board):
                 solution.append([r, c])
                 break
     return (solution)
+
 
 def xout(board, row, col):
     """X out spots on a chessboard.
@@ -95,6 +98,7 @@ def xout(board, row, col):
         board[r][c] = "x"
         c -= 1
 
+
 def recursive_solve(board, row, queens, solutions):
     """Recursively solve an N-queens puzzle.
 
@@ -117,20 +121,22 @@ def recursive_solve(board, row, queens, solutions):
             xout(tmp_board, row, c)
             solutions = recursive_solve(tmp_board, row + 1,
                                         queens + 1, solutions)
+
     return (solutions)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-        if sys.argv[1].isdigit() is False:
-            print("N must be a number")
-            sys.exit(1)
-        if int(sys.argv[1]) < 4:
-            print("N must be at least 4")
-            sys.exit(1)
+    if sys.argv[1].isdigit() is False:
+        print("N must be a number")
+        sys.exit(1)
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+        sys.exit(1)
 
-        board = init_board(int(sys.argv[1]))
-        solutions = recursive_solve(board, 0, 0, [])
-        for sol in solutions:
-            print(sol)
+    board = init_board(int(sys.argv[1]))
+    solutions = recursive_solve(board, 0, 0, [])
+    for sol in solutions:
+        print(sol)
