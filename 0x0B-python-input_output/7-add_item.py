fe8fe3a7c820dbf76fc4246ed6d04e4829b9f514
“sadewale72” <adewaleaderoju202@yahoo.com>
2022-10-25 05:06:07 -0700

    My eighth commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 from os import path
 from sys import argv
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if path.exists('add_item.json'):
     obj_json_file = load_from_json_file('add_item.json')
