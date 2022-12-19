6744176bf64cf818e8c2168174bb9bee6b105a7c
“sadewale72” <adewaleaderoju202@yahoo.com>
2022-12-19 05:47:48 -0800

    0x12-javascript-warm_up commit

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-console.log(line);
+const strings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (const string of strings) {
+	console.log(string);
 }
