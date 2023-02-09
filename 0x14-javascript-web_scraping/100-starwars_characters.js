9143299f78c31055bd2043ba923dc3f8fb47e8d9
sadewale72 <106751877+sadewale72@users.noreply.github.com>
2023-02-09 17:32:27 +0100

    first commit

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,20 +1,22 @@
 #!/usr/bin/node
-// Print all characters of a Star Wars movie
+
 const request = require('request');
-const url = `https://swapi.co/api/films/${process.argv[2]}`;
-request(url, { json: true }, (err, resp, body) => {
-  if (err) {
-    console.log(err);
-  } else if (body) {
-    const characters = body.characters;
-    for (const i in characters) {
-      request(characters[i], { json: true }, (err, resp, body) => {
-        if (err) {
-          console.log(err);
-        } else if (body) {
-          console.log(body.name);
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+let def = {};
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const abc = JSON.parse(body);
+    abc.characters.forEach(function (item, index, array) {
+      request(item, function (error, response, content) {
+        if (error) {
+          console.log(error);
+        } else {
+          def = JSON.parse(content);
+          console.log(def.name);
         }
       });
-    }
+    });
   }
 });
